4fa20b486185f51930d349e2c64389d0d66e9b0d
TUBryanN <117498676+TUBryanN@users.noreply.github.com>
2022-11-09 22:44:18 -0500

    Add files via upload

---
+++ project proposal.py
@@ -0,0 +1,39 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+# The title of my project will be likely called "Uno Game"
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+# The project I am presenting is an UNO card game. The objective of the game is player starting with seven cards and the first one to finish all the cards, wins! 
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+# Once run the program, the software will give you the set of 7 cards to each player that will be playing! It will randomly start with any card then it will list the card you have. This will allow the player if they would want to make a move to hit if they have a card, or pull from the deck randomly.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+TODO, if applicable
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+TODO, if applicable
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+# I am hoping that I am able to have fundamentals of the software that I want to have implement in the program. As long I have an output once I ran the program, then it is fine.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+# My goal is to fnish the project by the deadline, if not I can explain what the program does and what the expected outcome to be.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+# The best outcome for the final project is having the game to be setup for the player
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+# The steps that I will implement is using resources from class as well watching youtube videos to guide me through the way
